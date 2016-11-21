87275e3fc3d3439b5005751ab1dbfaac7d22d027
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-11-20 22:39:11 -0300

    Se agrega Vert.x Web y se configura el Router

--- primer-vertx/src/main/java/com/aantivero/MiPrimerVerticle.java
+++ primer-vertx/src/main/java/com/aantivero/MiPrimerVerticle.java
@@ -2,6 +2,8 @@ package com.aantivero;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 /**
  * Primer aplicacion vertx.
@@ -14,12 +16,21 @@ public class MiPrimerVerticle extends AbstractVerticle{
     //5 el Future se utiliza para indicar si fue completado o no
     @Override
     public void start(Future<Void> future) {
+        //crear el objeto Router
+        //Es el responsable de enviar las solicitudes HTTP al controlador correcto
+        Router router = Router.router(vertx);
+
+        //se realiza el Bind de '/' para que siga siendo compatible
+        router.route("/").handler(routingContext -> { //lambda
+            HttpServerResponse response = routingContext.response();
+            response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>Mi Primer Aplicacion con Vert.x</h1>");
+        });
         //2 al extender de AbstractVerticle ya tengo la propiedad vertx
         vertx
                 .createHttpServer() //6 se crea un HTTP Server
-                .requestHandler(r -> { //7 se le incorpora un handler que en este caso sera un lambda
-                    r.response().end("<h1>Mi Primer Aplicacion con Vert.x</h1>"); //8 lambda qe se ejecuta ante cada request
-                })
+                .requestHandler(router::accept) //7 pasa el método "accept" al request handler
                 .listen(
                     config().getInteger("http.port", 8080), //11 el puerto se determina por configuración
                     result -> { //9 se configura el puerto de escucha
